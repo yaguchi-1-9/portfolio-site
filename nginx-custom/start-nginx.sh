b458459af7d23efd6885155400824578250207bb
yaguchi-1-9 <yaguchitkg@gmail.com>
2023-10-22 03:40:02 +0900

    sed修正

--- nginx-custom/start-nginx.sh
+++ nginx-custom/start-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 環境変数PORTを使用して、Nginxの設定を動的に書き換える
-sed -i 's/listen $PORT;/listen '"$PORT"';/g' /etc/nginx/conf.d/default.conf
+sed -i 's/listen \$PORT;/listen '"$PORT"';/g' /etc/nginx/conf.d/default.conf
 
 # その後、Nginxと他のサービスを起動するコマンドを記述
 nginx -g 'daemon off;'
