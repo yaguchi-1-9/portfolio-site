5c2f414271dc61e5e8905c3258ce9feb5a0666c9
yaguchi-1-9 <yaguchitkg@gmail.com>
2023-10-22 03:56:21 +0900

    デバッグ用出力

--- nginx-custom/start-nginx.sh
+++ nginx-custom/start-nginx.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+echo "~~~~~~~~~~~~~~~ start-nginx.sh script is running ~~~~~~~~~~~~~~~"
+echo ""
+echo ""
+echo "PORT value is $PORT"
+echo ""
+echo ""
+
 # 環境変数PORTを使用して、Nginxの設定を動的に書き換える
 sed -i 's/listen \$PORT;/listen '"$PORT"';/g' /etc/nginx/conf.d/default.conf
-error_log('PORT: ' . getenv('PORT'));
+
+echo "Updated Nginx configuration:"
+cat /etc/nginx/conf.d/default.conf
 
 # その後、Nginxと他のサービスを起動するコマンドを記述
 nginx -g 'daemon off;'
