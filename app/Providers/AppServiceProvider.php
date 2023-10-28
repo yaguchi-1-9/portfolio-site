38f88140fc8ea7fa4132ddb2c790e6dcf48d1d5a
yaguchi-1-9 <yaguchitkg@gmail.com>
2023-10-29 01:39:53 +0900

    設定を戻す

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        #if($this->app->environment('production')) {
+        #    URL::forceScheme('https');
+        #}
     }
 }
