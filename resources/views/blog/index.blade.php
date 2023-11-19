a5aaaa5aa1f1daf3e8113b4e6abc6a620eb617f7
yaguchi-1-9 <yaguchitkg@gmail.com>
2023-11-19 23:56:27 +0900

    ブログ追加

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ブログ記事一覧</title>
+    <link rel="stylesheet" href="{{ asset('css/blog/index.css') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+</head>
+<body>
+    <h1>ブログ記事一覧</h1>
+    <div>
+        @foreach ($blogPosts as $post)
+            <div style="margin-bottom: 20px;">
+                <h2>{{ $post->title }}</h2>
+                <p>{{ $post->description }}</p>
+                <p>詳細を見る</p>
+            </div>
+        @endforeach
+    </div>
+
+    <!-- フッター -->
+    <footer>
+        <a href="{{ route('portfolio') }}">ポートフォリオ</a>
+    </footer>
+</body>
+</html>
--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ポートフォリオ</title>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/portfolio/index.css') }}">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 </head>
 <body>
@@ -22,18 +22,26 @@
 
 <section id="about">
     <h2>About Me</h2>
-    <p>こんにちは！</p>
-    <p>ここは新卒1年目のエンジニアの取り組みを紹介するページです。</p>
-    <p>拙い点も多いですが、生暖かい目で見てください。</p>
+    <p>こんにちは！<br>
+        物理学専攻で大学院を修了後、23卒でWebエンジニアになった屋口正鷹と申します。
+    </p>
+    <p>
+        座右の銘は「真の怠け者であれ！」です。<br>
+        私の考える真の怠け者とは、目先のことを超越した高い視座を持ち、怠けるための努力を続けられる人間を指す言葉です。<br>
+        長期的な視点を持って怠けるためには努力を惜しまない人間こそが「真の怠け者」であり、目先の怠惰に逃げる人間は本当の意味で怠け者ではありません。<br>
+    </p>
+    <p>ここは新卒1年目のエンジニアの取り組みを紹介するページです。<br>
+        拙い点も多いですが、生暖かい目で見てください。</p>
 </section>
 
 <section id="projects">
     <h2>Projects</h2>
 
     <div class="project-card">
-        <h3>Project 1</h3>
-        <p>プロジェクト1の説明。これは、...</p>
-        <a href="#">プロジェクトを見る</a>
+        <h3>個人ブログ</h3>
+        <p>人生の指針を忘れないようにしておくための個人ブログです。<br>
+            自分にとって大事な考え方や刺激を受けた考え方を言語化して、やりたいこと・やるべきことを見失ったときに見返すためのものです。</p>
+        <a href="{{ route('blog') }}">プロジェクトを見る</a>
     </div>
 
     <div class="project-card">
